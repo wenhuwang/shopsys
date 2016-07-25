c933118de3c59ef2d64c3ac1284b624e96878425
Wenhu Wang <976400757@qq.com>
2016-07-25 20:17:16 +0800

    更改数据库引擎为Mysql

--- shopsys/settings.py
+++ shopsys/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'shopsys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+	'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shopsys',
+        'USER': 'shopsys',
+        'PASSWORD': '123456789',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',    	
+	}
 }
 
 
